b0231905fe4a4290403da6a40de191f12b736906
SahilKumar-064738 <sahilkumar064738@gmail.com>
2025-08-13 20:51:08 +0530

    done main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import ConferenceTheme from "./pages/conference-theme";
 import PaperSubmission from "./pages/page-submission";
 import "./index.css";
 import RegistrationDetails from "./components/Registration/Registration-details";
+import About from "./pages/About";
+import OrganizingCommittee from "./pages/OrganizingCommittee";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,11 +17,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<NoPage />} />
+          <Route path="about" element={<About />} />
           <Route path="theme" element={<ConferenceTheme />} />
           <Route path="dates" element={<NoPage />} />
           <Route path="advisory-committee" element={<NoPage />} />
-          <Route path="organising-committee" element={<NoPage />} />
+          <Route path="organising-committee" element={<OrganizingCommittee />} />
           <Route path="keynote-speakers" element={<NoPage />} />
           <Route path="conference-tracks" element={<NoPage />} />
           <Route path="registration-details" element={<RegistrationDetails/>} />
